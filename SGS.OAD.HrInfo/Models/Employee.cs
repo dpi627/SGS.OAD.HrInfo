73edf2d44179355ff8ec2dfadebfcb7a15dd1805
brian_li <brian.li@sgs.com>
2025-02-04 15:36:45 +0800

    feat: 新增 Employee 類別至 SGS.OAD.HrInfo 命名空間
    - 新增 Employee 類別，包含多個員工資訊屬性
    - 屬性包括 StfCode, StfDept, Cmp, CmpName, DeptName, Unitcd, Unitname, AreaName, AreaNo, StfCname, StfEname, StfCome, QuitDate, Email, CostType, TitleName, TitleNameC, WorkType, SexType, ADAccount, BusinessUnit, WorkPlaceCode, WorkPlaceName, SuperEmpNo, SuperEmpCName, SuperEmpName, SuperEmpEmail, JobStatus, JobStatusText
    - 在檔案開頭加入 #nullable disable 指令

---
+++ SGS.OAD.HrInfo/Models/Employee.cs
@@ -0,0 +1,65 @@
+﻿#nullable disable
+
+namespace SGS.OAD.HrInfo
+{
+    public class Employee
+    {
+        public string StfCode { get; set; }
+
+        public string StfDept { get; set; }
+
+        public string Cmp { get; set; }
+
+        public string CmpName { get; set; }
+
+        public string DeptName { get; set; }
+
+        public string Unitcd { get; set; }
+
+        public string Unitname { get; set; }
+
+        public string AreaName { get; set; }
+
+        public string AreaNo { get; set; }
+
+        public string StfCname { get; set; }
+
+        public string StfEname { get; set; }
+
+        public DateTime? StfCome { get; set; }
+
+        public DateTime? QuitDate { get; set; }
+
+        public string Email { get; set; }
+
+        public string CostType { get; set; }
+
+        public string TitleName { get; set; }
+
+        public string TitleNameC { get; set; }
+
+        public string WorkType { get; set; }
+
+        public string SexType { get; set; }
+
+        public string ADAccount { get; set; }
+
+        public string BusinessUnit { get; set; }
+
+        public string WorkPlaceCode { get; set; }
+
+        public string WorkPlaceName { get; set; }
+
+        public string SuperEmpNo { get; set; }
+
+        public string SuperEmpCName { get; set; }
+
+        public string SuperEmpName { get; set; }
+
+        public string SuperEmpEmail { get; set; }
+
+        public string JobStatus { get; set; }
+
+        public string JobStatusText { get; set; }
+    }
+}
