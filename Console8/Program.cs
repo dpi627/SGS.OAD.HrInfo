7c884f7e58697cf5e0536592e2ea7eef566d23c9
brian_li <brian.li@sgs.com>
2025-02-04 15:38:09 +0800

    feat: 新增 SGS.OAD.HrInfo 套件及員工資料處理功能
    - 更新 `Console8.csproj` 檔案，新增 `SGS.OAD.HrInfo` 版本 `25.2.0` 的套件引用。
    - 修改 `Program.cs` 檔案：
      - 新增 `using SGS.OAD.HrInfo` 指令。
      - 更新 `Main` 方法：
        - 設定主控台輸入輸出編碼為 UTF-8。
        - 建立 `HrInfoHelper` 實例，並可選擇性提供連接字串或資料庫配置。
        - 使用 `GetByAdId` 和 `GetByEmpId` 方法檢索並列印員工資料。
        - 使用 `GetEmpId` 方法僅列印員工 ID。
      - 新增 `PrintData` 方法以列印物件屬性。

--- Console8/Program.cs
+++ Console8/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace Console8
+﻿using SGS.OAD.HrInfo;
+
+namespace Console8
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            // 可注入連線字串或指定伺服器與資料庫(二擇一)，或者直接使用目前預設值
+            var helper = HrInfoHelper.Create()
+                //.WithConnectionString("your private connection string")
+                //.WithDataBase("server name", "db name")
+                .Build();
+
+            Employee? emp;
+
+            // 透過 AD 帳號取得員工資料
+            emp = helper.GetByAdId("Nancy-Tw_Hu");
+            PrintData(emp);
+
+            // 透過員工編號取得員工資料
+            emp = helper.GetByEmpId("T18038");
+            PrintData(emp);
+
+            // 僅取員工編號
+            Console.WriteLine($"Get EmpId (staff code) only: {helper.GetEmpId("brian_li")}");
+        }
+
+        static void PrintData(object data)
+        {
+            foreach (var property in data.GetType().GetProperties())
+            {
+                Console.WriteLine($"{property.Name}: {property.GetValue(data)}");
+            }
+            Console.WriteLine($"{Environment.NewLine}");
         }
     }
 }
