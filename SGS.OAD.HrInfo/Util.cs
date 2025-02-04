7a5c9ea4d4524386efc01e9fac9ae00a6e2438d6
brian_li <brian.li@sgs.com>
2025-02-04 15:37:06 +0800

    feat: 新增命名空間與實用方法
    - 新增 `SGS.OAD.DB` 的 using 指令。
    - 引入新命名空間 `SGS.OAD.HrInfo`。
    - 在新命名空間中建立內部類別 `Util`。
    - 新增靜態方法 `GetConnectionString`，使用 `DbInfoBuilder` 建構並返回連接字串。
    - 新增靜態方法 `GetDateTime`，將物件轉換為 `DateTime?`，若物件不是 `DBNull.Value` 且其整數表示不為零，則使用 "yyyyMMdd" 格式進行轉換。
    - 新增靜態方法 `GetQuiteDate`，呼叫 `GetDateTime` 並將結果日期調整到一天的結束，通過加一天並減一秒來實現。

---
+++ SGS.OAD.HrInfo/Util.cs
@@ -0,0 +1,30 @@
+﻿using SGS.OAD.DB;
+
+namespace SGS.OAD.HrInfo;
+
+internal class Util
+{
+    public static string GetConnectionString(string server, string database, string appName = "SGS.OAD.HrInfo")
+    {
+        return DbInfoBuilder.Init()
+        .SetServer(server)
+        .SetDatabase(database)
+        .SetAppName(appName)
+        .Build()
+        .ConnectionString;
+    }
+
+    public static DateTime? GetDateTime(object val)
+    {
+        if (val != DBNull.Value && Convert.ToInt32(val) != 0)
+        {
+            return DateTime.ParseExact(val.ToString(), "yyyyMMdd", null);
+        }
+        return null;
+    }
+
+    public static DateTime? GetQuiteDate(object val)
+    {
+        return GetDateTime(val)?.AddDays(1).AddSeconds(-1);
+    }
+}
