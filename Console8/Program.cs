95176dc1be07d865bbdd7281db3494e80a03b2aa
brian_li <brian.li@sgs.com>
2025-02-05 08:35:23 +0800

    feat: 更新 `Main` 方法為非同步並新增 `TestAsync`
    - 將 `Console8.csproj` 中 `SGS.OAD.HrInfo` 套件版本從 `25.2.1` 更新至 `25.2.2`
    - 修改 `Program.cs` 中的 `Main` 方法為非同步，簽名變更為 `static async Task Main()`
    - 將同步測試代碼分離至新方法 `Test()`
    - 新增非同步方法 `TestAsync()` 處理非同步操作
    - `Main` 方法現在同時呼叫 `Test()` 和 `TestAsync()` 進行同步和非同步測試
    - `Test` 方法包含同步呼叫 `HrInfoHelper` 方法以獲取員工數據和員工 ID
    - `TestAsync` 方法包含非同步呼叫 `HrInfoHelper` 方法以獲取員工數據和員工 ID
    - `PrintData` 方法保持不變，用於列印員工數據屬性

--- Console8/Program.cs
+++ Console8/Program.cs
@@ -4,29 +4,47 @@ namespace Console8
 {
     internal class Program
     {
-        static void Main()
+        static async Task Main()
         {
             Console.InputEncoding = System.Text.Encoding.UTF8;
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-            // 可注入連線字串或指定伺服器與資料庫(二擇一)，或者直接使用目前預設值
-            var helper = HrInfoHelper.Create()
-                //.WithConnectionString("your private connection string")
-                //.WithDataBase("server name", "db name")
-                .Build();
+            // 測試同步
+            Test();
+            // 測試非同步
+            await TestAsync();
+        }
+
+        static void Test()
+        {
+            HrInfoHelper? helper = HrInfoHelper.Create().Build();
 
             Employee? emp;
-
             // 透過 AD 帳號取得員工資料
             emp = helper.GetByAdId("Nancy-Tw_Hu");
             PrintData(emp);
-
             // 透過員工編號取得員工資料
             emp = helper.GetByEmpId("T18038");
             PrintData(emp);
-
             // 僅取員工編號
-            Console.WriteLine($"Get EmpId (staff code) only: {helper.GetEmpId("brian_li")}");
+            string empId = helper.GetEmpId("brian_li");
+            Console.WriteLine($"EmpId: {empId}");
+        }
+
+        static async Task TestAsync()
+        {
+            HrInfoHelper? helper = await HrInfoHelper.Create().BuildAsync();
+
+            Employee? emp;
+            // 透過 AD 帳號取得員工資料
+            emp = await helper.GetByAdIdAsync("Nancy-Tw_Hu");
+            PrintData(emp);
+            // 透過員工編號取得員工資料
+            emp = await helper.GetByEmpIdAsync("T18038");
+            PrintData(emp);
+            // 僅取員工編號
+            string empId = await helper.GetEmpIdAsync("brian_li");
+            Console.WriteLine($"EmpId: {empId}");
         }
 
         static void PrintData(object data)
