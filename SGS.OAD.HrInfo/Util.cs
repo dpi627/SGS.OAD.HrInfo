339074ee8c71425de690790f84877ae36dee8871
brian_li <brian.li@sgs.com>
2025-02-05 08:47:23 +0800

    doc(Util): 新增技術文件說明

--- SGS.OAD.HrInfo/Util.cs
+++ SGS.OAD.HrInfo/Util.cs
@@ -4,10 +4,26 @@ namespace SGS.OAD.HrInfo;
 
 internal class Util
 {
+    /// <summary>
+    /// 獲取連接字串。
+    /// </summary>
+    /// <param name="server">伺服器名稱。</param>
+    /// <param name="database">資料庫名稱。</param>
+    /// <param name="appName">應用程式名稱，預設為 "SGS.OAD.HrInfo"。</param>
+    /// <returns>連接字串。</returns>
     public static string GetConnectionString(string server, string database, string appName = "SGS.OAD.HrInfo")
     {
         return GetConnectionStringAsync(server, database, appName).GetAwaiter().GetResult();
     }
+
+    /// <summary>
+    /// 非同步獲取連接字串。
+    /// </summary>
+    /// <param name="server">伺服器名稱。</param>
+    /// <param name="database">資料庫名稱。</param>
+    /// <param name="appName">應用程式名稱，預設為 "SGS.OAD.HrInfo"。</param>
+    /// <param name="cancellationToken">取消操作的標記。</param>
+    /// <returns>連接字串的非同步任務。</returns>
     public static async Task<string> GetConnectionStringAsync(string server, string database, string appName = "SGS.OAD.HrInfo", CancellationToken cancellationToken = default)
     {
         return await DbInfoBuilder.Init()
@@ -18,6 +34,11 @@ internal class Util
             .ContinueWith(t => t.Result.ConnectionString);
     }
 
+    /// <summary>
+    /// 獲取日期時間。
+    /// </summary>
+    /// <param name="val">日期時間的物件。</param>
+    /// <returns>如果值有效，則返回日期時間；否則返回 null。</returns>
     public static DateTime? GetDateTime(object val)
     {
         if (val != DBNull.Value && Convert.ToInt32(val) != 0)
@@ -27,6 +48,11 @@ internal class Util
         return null;
     }
 
+    /// <summary>
+    /// 獲取結束日期時間。
+    /// </summary>
+    /// <param name="val">日期時間的物件。</param>
+    /// <returns>如果值有效，則返回結束日期時間；否則返回 null。</returns>
     public static DateTime? GetQuiteDate(object val)
     {
         return GetDateTime(val)?.AddDays(1).AddSeconds(-1);
