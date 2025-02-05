9f673491a3df4c30f338d1217d6f4dc19d7beb91
brian_li <brian.li@sgs.com>
2025-02-05 08:35:39 +0800

    feat: 更新 SGS.OAD.HrInfo 套件版本及 Main 方法
    - 將 Console48.csproj 中 SGS.OAD.HrInfo 套件版本從 25.2.0.0 更新至 25.2.2.0，並更新 HintPath 指向新版本的 DLL。
    - 將 Program.cs 中的 Main 方法改為非同步，簽名變更為 static async Task Main()。
    - 新增同步方法 Test 和非同步方法 TestAsync，Test 方法呼叫 HrInfoHelper.Create().Build()，TestAsync 方法呼叫 HrInfoHelper.Create().BuildAsync(cancellationToken)，並包含預設值的 CancellationToken 參數。
    - 更新 packages.config 中 SGS.OAD.HrInfo 套件版本從 25.2.0 更新至 25.2.2。

--- Console48/Program.cs
+++ Console48/Program.cs
@@ -1,14 +1,28 @@
 ﻿using SGS.OAD.HrInfo;
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Console48
 {
     internal class Program
     {
-        static void Main()
+        static async Task Main()
+        {
+            Test();
+            await TestAsync();
+        }
+
+        static void Test()
         {
             var helper = HrInfoHelper.Create().Build();
             Console.WriteLine($"EmpId: {helper.GetEmpId("brian_li")}");
+        }
+
+        static async Task TestAsync(CancellationToken cancellationToken = default)
+        {
+            var helper = await HrInfoHelper.Create().BuildAsync(cancellationToken);
+            Console.WriteLine($"EmpId: {await helper.GetEmpIdAsync("brian_li")}");
         }
     }
 }
